d0dcbcda28b87a1ccfc5cf2b88081a25610e3ce6
Priti Saroj <105093924+Pritisaroj123@users.noreply.github.com>
2025-04-25 09:55:02 +0530

    Update LectureTab.jsx

--- client/src/pages/admin/lecture/LectureTab.jsx
+++ client/src/pages/admin/lecture/LectureTab.jsx
@@ -17,7 +17,9 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 
-const MEDIA_API = "http://localhost:8080/api/v1/media";
+const url = "https://lms-mern-project-hzmu.onrender.com"
+
+const MEDIA_API = `${url}/api/v1/media`;
 
 const LectureTab = () => {
   const [lectureTitle, setLectureTitle] = useState("");
